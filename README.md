abb9db1241c4c6933ea63bc90e66dbc4e882187e
Paul Farino <pivotal@pivotal-wifi-2-33.ny.pivotallabs.com>
2015-04-23 11:22:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sketch Device Collection
+
+This plugin makes it easy to to fill layers in Sketch with devices. 
+
+#### Current supported devices: 
+- iPhone 5
+- iPhone 6
+- Nexus 5
+
+## Installation
+1. Open Sketch and choose Plugins > Reveal Plugins Folder
+1. [Download this project](https://github.com/paulfarino/sketch-device-collection/archive/master.zip) and place the folder inside the Plugins directory.
+1. Select some layers in Sketch and run the plugin from the Plugins menu.
+
+or 
+
+Or clone it yourself:
+
+    $ git clone https://github.com/paulfarino/sketch-device-collection.git
+
+-------------
+Brought to you by [Paul Farino](https://github.com/paulfarino)
+Based on the work of [ImageFiller for Sketch](https://github.com/awt2542/ImageFiller)
+iOS Devices by [Robbie Pearce](http://robbiepearce.com/devices)
+Nexus5 Device by [Tristan Remy](http://tristanremy.com/nexus-5/)
+
+
---
+++ _lib.js
@@ -0,0 +1,56 @@
+#import '_sandbox.js'
+var pluginPath = sketch.scriptPath.match( /(?:.*)(?:\/Plugins\/)(?:[^\/]*)(?:)/gm ); // ImageFiller's folder
+
+function showError(msg){
+  doc.showMessage(msg);
+  return false;
+}
+
+function getImagesFromPath(imagesPath){
+
+  if (imagesPath.slice(-1) != "/") imagesPath = imagesPath+"/"; // ugly fix for path concatenations
+  new AppSandbox().authorize(imagesPath, run);
+  
+  function run(){
+    
+    var usedImages = [];
+    var selections = selection.count();
+    if (selections == 0) showError("Oh! You need to select a few layers before running the plugin.");
+    
+    // Get all images from the folder and its subdirectories
+    var fileManager = NSFileManager.defaultManager();
+    fileManager.changeCurrentDirectoryPath(imagesPath);
+    var enumerator = fileManager.enumeratorAtPath(imagesPath);
+    var dirsNFiles = [[NSMutableArray alloc] init];
+    while ((entry = [enumerator nextObject]) != null){
+       if (fileManager.fileExistsAtPath(entry)) {
+         dirsNFiles.addObject(entry);
+       }
+    }
+    var extensions = [NSArray arrayWithObjects:@"png", @"PNG", @"jpg", @"JPG", @"jpeg", @"JPEG", @"gif", @"GIF", nil];
+    var imagesFileNames = [dirsNFiles filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"pathExtension IN %@", extensions]];
+    var imgLen = imagesFileNames.count();
+    if (imgLen == 0) showError("Hm. Couldn't find any images inside "+imagesPath+". You should add a few.");
+    
+    // apply images to selection
+    var theimages = [[NSMutableArray alloc] init];
+    theimages.addObjectsFromArray(imagesFileNames);
+
+    for (var i=0; i < selections; i++){
+      if (theimages.count() == 0) theimages.addObjectsFromArray(imagesFileNames);
+      var layer = selection[i]
+      var fills = layer.style().fills()
+      if (fills.count() == 0) fills.addNewStylePart();
+      var firstFill = fills.lastObject();
+      firstFill.setFillType(4); 
+      firstFill.setPatternFillType(1);
+      var r = Math.floor(Math.random() * theimages.count());
+      var fileName = imagesPath+theimages[r];
+      if (fileManager.fileExistsAtPath(fileName)) {
+        image = [[NSImage alloc] initWithContentsOfFile:fileName];
+        firstFill.setPatternImage(image);
+      }
+      theimages.removeObjectAtIndex(r)
+    }
+  }
+}
